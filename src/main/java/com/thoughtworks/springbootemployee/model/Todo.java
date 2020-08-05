dac2a55d6189d02d9c9f2b2e92f351914a6d934b
hardy <hard.t.z.wei@oocl.com>
2020-08-05 16:38:28 +0800

    [ToDo_Server][Hardy] Feature : Add todoModel classes

--- src/main/java/com/thoughtworks/springbootemployee/model/Todo.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Todo.java
@@ -1,4 +1,43 @@
 package com.thoughtworks.springbootemployee.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+    private Boolean completed;
+
+    public Todo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
 }
